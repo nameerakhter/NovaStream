7d96e1004b789e4914a0c2e499b6b54c9ad5e371
nameerakhter <akhtarnameer@gmail.com>
2025-01-09 15:27:50 +0530

    chore: add logo to Navbar

--- src/app/(browse)/_components/navbar/index.tsx
+++ src/app/(browse)/_components/navbar/index.tsx
@@ -1,7 +1,10 @@
+import { Logo } from "./logo";
+
 export default function Navbar() {
 	return (
 		<nav>
-			<div className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
+			<div className="fixed top-0 w-full h-20 z-[49]  px-2 lg:px-4 flex justify-between items-center shadow-sm">
+				<Logo />
 				Navbar
 			</div>
 		</nav>
