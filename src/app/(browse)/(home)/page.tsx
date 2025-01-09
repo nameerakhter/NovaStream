5c239352a8bd44fc057614507c3a0b9a07f22346
nameerakhter <akhtarnameer@gmail.com>
2025-01-09 13:36:25 +0530

    refactor: home page + add navbar component

---
+++ src/app/(browse)/(home)/page.tsx
@@ -0,0 +1,17 @@
+
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<nav className="w-full p-2">
+				<UserButton afterSignOutUrl="/sign-in" />
+			</nav>
+			<h1>Home page</h1>
+		</div>
+	);
+}
+
+
+
+
