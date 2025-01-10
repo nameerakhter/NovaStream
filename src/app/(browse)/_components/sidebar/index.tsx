8c9cb0cd44dbccdd359fef1675424f38c85c164d
nameerakhter <akhtarnameer@gmail.com>
2025-01-10 08:18:58 +0530

    feat: add recommended user to sidebar

--- src/app/(browse)/_components/sidebar/index.tsx
+++ src/app/(browse)/_components/sidebar/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-//import { getRecommended } from "@/lib/recommended-service";
+import { getRecommended } from "@/lib/recommended-service";
 //import { getFollwedUser } from "@/lib/follow-service";
 
 import { Wrapper } from "./wrapper";
 import { Toggle, ToggleSkeleton } from "./toggle";
-//import { Recommended, RecommendedSkeleton } from "./recommended";
+import { Recommended, RecommendedSkeleton } from "./recommended";
 //import { Following, FollowingSkeleton } from "./following";
 
 export async function Sidebar() {
-	//const recommended = await getRecommended();
+	const recommended = await getRecommended();
 	//const following = await getFollwedUser();
 
 	return (
@@ -17,7 +17,7 @@ export async function Sidebar() {
 			<Toggle />
 			<div className="space-y-4 pt-4 lg:pt-0">
 				{/*<Following data={following} />*/}
-				{/*<Recommended data={recommended} />*/}
+				<Recommended data={recommended} />
 			</div>
 		</Wrapper>
 	);
@@ -28,7 +28,7 @@ export function SidebarSkeleton() {
 		<aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35 z-50]">
 			<ToggleSkeleton />
 			{/*<FollowingSkeleton />*/}
-			{/*<RecommendedSkeleton />*/}
+			<RecommendedSkeleton />
 		</aside>
 	);
 }
