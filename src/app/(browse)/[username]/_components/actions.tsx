6976f011acf3bc78f4fb3829617c17b46005cbf1
nameerakhter <akhtarnameer@gmail.com>
2025-01-13 10:27:57 +0530

    feat: add unfollow service

--- src/app/(browse)/[username]/_components/actions.tsx
+++ src/app/(browse)/[username]/_components/actions.tsx
@@ -1,10 +1,19 @@
 "use client"
+import { onFollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 
 export default function Actions() {
+	const [isPending, startTransition] = useTransition()
+	function onClick() {
+		startTransition(() => {
+
+			onFollow("123")
+		})
+	}
 	return (
-		<Button variant="primary">
+		<Button disabled={isPending} onClick={onClick} variant="primary">
 			Follow
 		</Button >
 	)
--- src/lib/follow-service.ts
+++ src/lib/follow-service.ts
@@ -69,3 +69,37 @@ export async function followUser(id: string) {
 
 	return follow
 }
+
+export async function unFollowUser(id: string) {
+	const self = await getSelf()
+
+	const otherUser = await db.user.findFirst({
+		where: { id }
+	})
+
+	if (!otherUser) {
+		throw new Error("User not found")
+	}
+
+	if (otherUser.id === self.id) throw new Error("You can't unfollow yourself");
+
+	const existingFollow = await db.follow.findFirst({
+		where: {
+			followerId: self.id,
+			followingId: otherUser.id,
+		},
+	});
+
+	if (!existingFollow) throw new Error("You are not following this user");
+
+	const follow = await db.follow.delete({
+		where: {
+			id: existingFollow.id,
+		},
+		include: {
+			following: true,
+		},
+	});
+
+	return follow;
+}
