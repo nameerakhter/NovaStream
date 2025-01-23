f307d14f0c86179f9c2d04d080e31de1f2caaf2f
nameerakhter <akhtarnameer@gmail.com>
2025-01-23 17:11:31 +0530

    fix: only return JSON data in ingress

--- src/actions/ingress.ts
+++ src/actions/ingress.ts
@@ -16,30 +16,39 @@ import { revalidatePath } from "next/cache";
 import { db } from "@/lib/db";
 import { getSelf } from "@/lib/auth-service";
 
+
 const roomService = new RoomServiceClient(
   process.env.LIVEKIT_API_URL!,
   process.env.LIVEKIT_API_KEY!,
   process.env.LIVEKIT_API_SECRET!
 );
 
-const ingressClient = new IngressClient(process.env.LIVEKIT_API_URL!);
+const ingressClient = new IngressClient(process.env.NEXT_PUBLIC_LIVEKIT_WS_URL!, process.env.LIVEKIT_API_KEY!, process.env.LIVEKIT_API_SECRET!);
+
 
 export async function resetIngresses(hostId: string) {
-  const ingresses = await ingressClient.listIngress({
-    roomName: hostId,
-  });
+  try {
+    const ingresses = await ingressClient.listIngress({
+      roomName: hostId,
+    });
 
-  const rooms = await roomService.listRooms([hostId]);
+    const rooms = await roomService.listRooms([hostId]);
+    console.log(rooms)
 
-  for (const room of rooms) {
-    await roomService.deleteRoom(room.name);
-  }
-
-  for (const ingress of ingresses) {
-    if (ingress.ingressId) {
-      await ingressClient.deleteIngress(ingress.ingressId);
+    for (const room of rooms) {
+      await roomService.deleteRoom(room.name);
     }
+
+    for (const ingress of ingresses) {
+      if (ingress.ingressId) {
+        await ingressClient.deleteIngress(ingress.ingressId);
+      }
+    }
+  } catch (error) {
+    console.error("Error resetting ingresses:", error);
+    throw new Error("Failed to reset ingresses");
   }
+
 };
 
 export async function createIngress(ingressType: IngressInput) {
@@ -54,6 +63,7 @@ export async function createIngress(ingressType: IngressInput) {
     participantIdentity: self.id,
   };
 
+  console.log(options)
   if (ingressType === IngressInput.WHIP_INPUT) {
     options.enableTranscoding = true
   } else {
@@ -90,6 +100,12 @@ export async function createIngress(ingressType: IngressInput) {
     },
   });
 
+  const ingressData = {
+    ingressId: ingress.ingressId,
+    serverUrl: ingress.url,
+    streamKey: ingress.streamKey,
+  };
+
   revalidatePath(`/u/${self.username}/keys`);
-  return ingress;
+  return ingressData;
 };
