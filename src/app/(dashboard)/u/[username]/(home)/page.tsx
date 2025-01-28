f30301c11b31dc77981bac00a83cadb50b49ea58
nameerakhter <akhtarnameer@gmail.com>
2025-01-28 09:05:39 +0530

    chore: add streamPlayer props

--- src/app/(dashboard)/u/[username]/(home)/page.tsx
+++ src/app/(dashboard)/u/[username]/(home)/page.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { curentUser } from "@clerk/nextjs";
 
 import { getUserByUsername } from "@/lib/user-service";
-import { StreamPlayer } from "@/components/stream-player";
+import {StreamPlayer} from "@/components/stream-player";
 
 export default async function CreatorPage({
   params: { username },
 }: {
   params: { username: string };
 }) { 
-	
-  const externalUser = await currentUser();
+
+  const externalUser = await currentUser();//fix: this from clerk
   const user = await getUserByUsername(username);
 
   if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
--- src/components/stream-player.tsx
+++ src/components/stream-player.tsx
@@ -1,4 +1,15 @@
-export  function StreamPlayer({user, stream, isFollowing}){
+"use client"
+
+import { User } from "@clerk/nextjs/server"
+import { Stream } from "stream"
+
+interface StreamPlayerProps{
+    user: User & {stream: Stream | null}
+    stream: Stream
+    isFollowing: boolean
+}
+
+export  function StreamPlayer({user, stream, isFollowing}:StreamPlayerProps){
     return(
         <div>Stream Player</div>
     )
