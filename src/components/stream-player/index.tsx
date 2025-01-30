b1fa4da75b0e1bc5b9038d76717dc3eb4de80462
nameerakhter <akhtarnameer@gmail.com>
2025-01-30 12:32:12 +0530

    feat: add skeletons to stream-player

--- src/components/stream-player/index.tsx
+++ src/components/stream-player/index.tsx
@@ -1,14 +1,26 @@
 "use client"
 
 import { User } from "@clerk/nextjs/server"
-import { Stream } from "stream"
+
 import { LiveKitRoom } from "@livekit/components-react"
 import { useViewerToken } from "../../../hooks/use-viewer-token"
-import { Video } from "./video"
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
-import { Chat } from "./chat"
+import { Room } from "livekit-client";
+import { Video, VideoSkeleton } from "./video";
+import { Chat, ChatSkeleton } from "./chat";
+import { Header, HeaderSkeleton } from "./header";
 
+
+interface Stream {
+	id: string;
+	isChatEnabled: boolean;
+	isChatDelayed: boolean;
+	isChatFollowersOnly: boolean;
+	isLive: boolean;
+	thumbnailUrl: string | null;
+	name: string;
+}
 
 interface StreamPlayerProps {
 	user: User & { stream: Stream | null }
@@ -30,6 +42,7 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
 			<div>Cannot find user</div>
 		)
 	}
+	console.log("stream", stream)
 	return (
 		<LiveKitRoom
 			token={token}
@@ -39,7 +52,7 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
 				collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
 			  )}		>
 			<div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
-				<Video hostName={user.username} hostIdentity={user.id} stream={stream}/>
+				<Video hostName={user.username} hostIdentity={user.id} room={new Room(stream)}/>
 			</div>
 			<div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
@@ -55,3 +68,17 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
 		</LiveKitRoom>
 	)
 }
+
+export function StreamPlayerSkeleton() {
+	return (
+	  <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+		<div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+		  <VideoSkeleton />
+		  <HeaderSkeleton />
+		</div>
+		<div className="col-span-1 bg-background">
+		  <ChatSkeleton />
+		</div>
+	  </div>
+	);
+  }
