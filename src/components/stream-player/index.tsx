3fd5e6132767842d57707fba02b58f847a03d57e
nameerakhter <akhtarnameer@gmail.com>
2025-01-30 12:47:34 +0530

    feat: add Header to streamPlayer component

--- src/components/stream-player/index.tsx
+++ src/components/stream-player/index.tsx
@@ -37,7 +37,7 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
 			<div>Cannot watch the stream</div>
 		)
 	}
-	if(!user || user.username === null){
+	if (!user || user.username === null) {
 		return (
 			<div>Cannot find user</div>
 		)
@@ -50,35 +50,43 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
 			className={cn(
 				"grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
 				collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
-			  )}		>
+			)}		>
 			<div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
-				<Video hostName={user.username} hostIdentity={user.id} room={new Room(stream)}/>
+				<Video hostName={user.username} hostIdentity={user.id} room={new Room(stream)} />
+				<Header
+					imageUrl={user.imageUrl}
+					hostName={user.username}
+					hostIdentity={user.id}
+					isFollowing={isFollowing}
+					name={stream.name}
+					viewerIdentity={identity}
+				/>
 			</div>
 			<div className={cn("col-span-1", collapsed && "hidden")}>
-          <Chat
-            viewerName={name}
-            hostName={user.username}
-            hostIdentity={user.id}
-            isFollowing={isFollowing}
-            isChatEnabled={stream.isChatEnabled}
-            isChatDelayed={stream.isChatDelayed}
-            isChatFollowersOnly={stream.isChatFollowersOnly}
-          />
-        </div>
+				<Chat
+					viewerName={name}
+					hostName={user.username}
+					hostIdentity={user.id}
+					isFollowing={isFollowing}
+					isChatEnabled={stream.isChatEnabled}
+					isChatDelayed={stream.isChatDelayed}
+					isChatFollowersOnly={stream.isChatFollowersOnly}
+				/>
+			</div>
 		</LiveKitRoom>
 	)
 }
 
 export function StreamPlayerSkeleton() {
 	return (
-	  <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
-		<div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
-		  <VideoSkeleton />
-		  <HeaderSkeleton />
+		<div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+			<div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+				<VideoSkeleton />
+				<HeaderSkeleton />
+			</div>
+			<div className="col-span-1 bg-background">
+				<ChatSkeleton />
+			</div>
 		</div>
-		<div className="col-span-1 bg-background">
-		  <ChatSkeleton />
-		</div>
-	  </div>
 	);
-  }
+}
