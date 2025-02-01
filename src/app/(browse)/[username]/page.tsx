0138d1f40637609d95f4a5c65739b52145722b2a
nameerakhter <akhtarnameer@gmail.com>
2025-02-01 15:39:32 +0530

    feat: update user page to display stream

--- src/app/(browse)/[username]/page.tsx
+++ src/app/(browse)/[username]/page.tsx
@@ -1,35 +1,39 @@
-import { isFollowingUser } from "@/lib/follow-service";
-import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
-import Actions from "./_components/actions";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
 import { isBlockedByUser } from "@/lib/block-service";
+import { StreamPlayer } from "@/components/stream-player";
 
-//interface UserPageProps {
-//  params: { username: string };
-//}
-//
-export default async function userPage({ params }: { params: Promise<{ username: string }> }) {
-  const awaitedParams = (await params).username
-  const user = await getUserByUsername(awaitedParams)
+interface UserPageProps {
+  params: {
+    username: string;
+  };
+};
 
-  if (!user) {
-    notFound()
+const UserPage = async ({
+  params
+}: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user || !user.stream) {
+    notFound();
   }
 
-  const isFollowing = await isFollowingUser(user.id)
-  const isBlockedByThisUser = await isBlockedByUser(user.id)
+  const isFollowing = await isFollowingUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
 
-  return (
-    <div className="flex flex-col gap-y-4">
-      <p>username: {user.username}</p>
-      <p>userID: {user.id}</p>
-      <p>isFollowing: {`${isFollowing}`}</p>
-      <p>isBlockedByThisUser: {`${isBlockedByThisUser}`}</p>
-      <Actions
-        isBlocked={isBlockedByThisUser}
-        userId={user.id}
-        isFollowing={isFollowing}
-      />
-    </div>
-  )
+  if (isBlocked) {
+    notFound();
+  }
+
+  return ( 
+    <StreamPlayer
+      user={user}
+      stream={user.stream}
+      isFollowing={isFollowing}
+    />
+  );
 }
+ 
+export default UserPage;
