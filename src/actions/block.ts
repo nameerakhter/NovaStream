b7f71dac007ded9612448a54616839a60a22426b
nameerakhter <akhtarnameer@gmail.com>
2025-02-01 15:38:52 +0530

    feat: updaet block user server action

--- src/actions/block.ts
+++ src/actions/block.ts
@@ -1,42 +1,43 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
 
-import { blockUser, unblockUser } from "@/lib/block-service";
+import { getSelf } from "@/lib/auth-service";
+import { blockUser, unblockUser } from "@/lib/block-service"
 
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!,
+);
 
 export const onBlock = async (id: string) => {
-  //TODO: Adapt to disconnect from livestream
-  //TODO: Allow to kick the guest
+  const self = await getSelf();
+
+  let blockedUser;
+
   try {
-    const blockedUser = await blockUser(id)
-    revalidatePath("/")
-
-    if (blockedUser) {
-      revalidatePath(`/${blockedUser.blocked.username}`)
-    }
-
-    return blockedUser;
-
-  } catch (error) {
-    console.log(error)
-    throw new Error("Internal server error");
+    blockedUser = await blockUser(id);
+  } catch {
+    // This means user is a guest
   }
+
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    // This means user is not in the room
+  }
+
+  revalidatePath(`/u/${self.username}/community`);
+
+  return blockedUser;
 };
 
 export const onUnblock = async (id: string) => {
-  try {
-    const unblockedUser = await unblockUser(id);
-    revalidatePath("/")
+  const self = await getSelf();
+  const unblockedUser = await unblockUser(id);
 
-    if (unblockedUser) {
-      revalidatePath(`/${unblockedUser.blocked.username}`)
-    }
-    return unblockedUser;
-
-  } catch (error) {
-
-    console.log(error)
-    throw new Error("Internal server error");
-  }
+  revalidatePath(`/u/${self.username}/community`);
+  return unblockedUser;
 };
